23594ab2d9a984de23bf8a3b4aef8cc015d32494
Ariel Santos <ariel.santos@dcc.ufmg.br>
2019-07-20 03:00:05 -0300

    Manual de uso, script bruto

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # lancamentonotas-2019
 Aplicação provisória para lançamento de notas. 
+
+## Setup
+- Crie os containers
+    ```bash
+    docker-compose up -d
+    ```
+- Popule o banco de dados **apenas** com os alunos, turmas, atividades, etc relativas ao semestre atual (e somente as tableas inclusas em [models.py](./lancanotas/mainapp/models.py))
+    ```bash
+    docker exec -i lancamentonotas-2019_internaldb_1 sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"' < database.sql
+    ```
+- Setup do Django (dentro do container-serviço *app*)
+    ```bash
+    cd lancanotas
+    python manage.py migrate
+    ```
+- Pronto para executar o comando!
+
+## Executando o comando
+
+O comando ```python manage.py importnotas``` lê um arquivo CSV com o nome do código de uma turma ('ART201', por ex.), devidamente formatado desta forma:
+```csv
+Nome do Aluno, [Atividade(s)], Nota Final
+João, [1,2...], 90
+```
+e insere as notas no banco de dados apropriadamente, ignorando a última coluna da nota final.
+
+
+Há também um argumento que pode ser adicionado ao final do comando: ```--final-only```. Ele fará com que só seja lançada a Nota Final especificada na última coluna do arquivo.
+
+## Use os dados no seu banco de dados
+Extraia os dados do container, usando ```docker exec -i lancamentonotas-2019_internaldb_1 sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > lancamentos.sql``` por exemplo, e utlize o SQL gerado para importar os dados onde quiser. Tenha atenção para não importar também as tabelas de autenticação geradas pelo Django (auth_* e django_*).
+
+
+Este programa não foi feito para uso regular, sem garantias de integridade de dados, etc.
--- lancanotas/mainapp/management/commands/importnotas.py
+++ lancanotas/mainapp/management/commands/importnotas.py
@@ -1,10 +1,19 @@
 import csv
 import io
+import os
 import sys
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import (BaseCommand, CommandError,
+                                         no_translations)
+from django.db import transaction
 
-from mainapp.models import Aluno
+from mainapp.models import (Aluno, Atividade, DatasFuncionamento, NotaAluno,
+                            Turma, TurmaAlunos, TurmaAtividades)
+
+date_default = DatasFuncionamento.objects.get(
+    data_funcionamento__year=2019, data_funcionamento__month=7, data_funcionamento__day=6)
+data_new = DatasFuncionamento.objects.get(
+    data_funcionamento__year=2019, data_funcionamento__month=7, data_funcionamento__day=20)
 
 
 class Command(BaseCommand):
@@ -15,28 +24,63 @@ class Command(BaseCommand):
     output_transaction = True
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', nargs='+',
+        parser.add_argument('csv_file',
                             type=open, help='Arquivo CSV')
+        parser.add_argument('--final-only', action='store_true',
+                            help='Lance apenas uma nota final')
 
+    @no_translations
     def handle(self, *args, **options):
-        csvfile = options['csv_file'][0]
+        csvfile = options['csv_file']
         has_header = csv.Sniffer().has_header(csvfile.readline())
         dialect = csv.Sniffer().sniff(csvfile.readline())
         csvfile.seek(0)
         data_reader = csv.reader(csvfile, dialect)
+        line_len = len(next(data_reader))
+        classentity = Turma.objects.get(
+            nome_turma=os.path.basename(csvfile.name)[:-4])
+        activities = list()
         if has_header:
-            data_reader.__next__()
-        line_len = len(data_reader.__next__())
-        ativ_num = line_len - 2
+            csvfile.seek(0)
+            headers = next(data_reader)
+            activities = self.get_activities(
+                headers[1:-1], classentity)
+        elif not options['final_only']:
+            raise CommandError(
+                'Erro: Impossível criar atividades intermediárias sem cabeçários')
         try:
-            for row in data_reader:
-                aluno = Aluno.objects.filter(nome_aluno=row[0].upper())
-                if aluno.exists() and len(row) == line_len:
-                    self.stdout.write('[%s] Processando aluno "%s":' %
-                                      (data_reader.line_num, aluno[0].nome_aluno))
-                else:
-                    self.stdout.write(self.style.WARNING(
-                        '[%s] Não contém um aluno válido, ignorando' % data_reader.line_num))
-        except csv.Error:
-            self.stdout.write(self.style.ERROR(
-                'Erro na linha %s' % data_reader.line_num))
+            with transaction.atomic():
+                for row in data_reader:
+                    aluno = Aluno.objects.filter(nome_aluno=row[0].upper())
+                    if aluno.exists() and len(row) == line_len:
+                        self.stdout.write(self.style.SUCCESS('[%s]\tProcessando aluno "%s":' %
+                                                             (data_reader.line_num, aluno[0].nome_aluno)))
+                        if not options['final_only']:
+                            for pos, activity in enumerate(activities):
+                                nota = NotaAluno(id_turma_aluno=TurmaAlunos.objects.get(id_turma=classentity, id_aluno=aluno[0]), id_atividades_turma=TurmaAtividades.objects.get(
+                                    id_turma=classentity, id_atividade=activity), valor_nota=row[pos+1])
+                                nota.save()
+                                self.stdout.write('\t\tNota: %s [%s], TurmaAluno: [%s], AtividadesTurma: [%s]' % (
+                                    nota.valor_nota, nota.id_nota, nota.id_turma_aluno.id_turma_aluno, nota.id_atividades_turma.id_atividades_turma))
+                        else:
+                            final_activity = Atividade(
+                                data_funcionamento=data_new, nome='Nota Final', valor_total=100)
+                            final_activity.save()
+                            activity_class = TurmaAtividades(
+                                id_turma=classentity, id_atividade=final_activity)
+                            activity_class.save()
+                            nota = NotaAluno(id_turma_aluno=TurmaAlunos.objects.get(
+                                id_turma=classentity, id_aluno=aluno[0]), id_atividades_turma=activity_class, valor_nota=row[-1])
+                            nota.save()
+                            self.stdout.write('\t\tAtividade Final: [%s], Nota: %s [%s], TurmaAluno: id[%s], AtividadesTurma: [%s]' % (
+                                final_activity.id_atividade, nota.valor_nota, nota.id_nota, nota.id_turma_aluno.id_turma_aluno, nota.id_atividades_turma.id_atividades_turma))
+                    else:
+                        self.stdout.write(self.style.WARNING(
+                            '[%s]\tLinha inválida, ignorando "%s"' % (data_reader.line_num, row[0])))
+        except:
+            raise CommandError(
+                'Erro na linha %s, nenhum dado foi escrito ao banco de dados' % data_reader.line_num)
+
+    def get_activities(self, activities, classentity):
+        return list(map(lambda actt: Atividade.objects.get(
+            nome=actt, data_funcionamento=date_default, turmaatividades__id_turma=classentity), activities))
