5004bd4137d8c464b7c3740a9b465fd9fcd57f22
Ariel Santos <ariel.santos@dcc.ufmg.br>
2019-07-25 02:19:08 +0000

    Adaptação para lançar apenas a nota final

--- lancanotas/mainapp/management/commands/extractxls.py
+++ lancanotas/mainapp/management/commands/extractxls.py
@@ -37,14 +37,16 @@ class Command(BaseCommand):
             turma = Turma.objects.get(
                 nome_turma=sheetname, id_periodo=periodo)
             self.stdout.write(self.style.SUCCESS(
-                '[1/5] Turma %s encontrada' % turma.nome_turma))
-            sheet = wb[sheetname]
+                'Turma encontrada %s:' % turma.nome_turma))
+
             # Checando se as âncoras do modelo padrão estão presentes
+            sheet = wb[sheetname]
             if not (sheet['B6'].value is not None and sheet['B6'].value.strip() == 'Nome da Atividade' and sheet['B8'].value is not None and sheet['B8'].value.strip() == 'Nome do Aluno'):
                 raise CommandError('Arquivo não conforma com modelo padrão!\n Âncoras: %s, %s' % (
                     sheet['B6'].value, sheet['B8'].value))
             self.stdout.write(self.style.SUCCESS(
-                '[2/5] Documento bem-formatado'))
+                '\t[1/7] Documento bem-formatado'))
+
             alunos = list()
             for num in itertools.count(10):
                 if sheet['B%d' % num].value is None:
@@ -56,58 +58,56 @@ class Command(BaseCommand):
                     aluno = TurmaAlunos.objects.get(
                         id_aluno__nome_aluno=value, id_turma=turma)
                     alunos.append({'ent': aluno, 'pos': num})
-                    self.stdout.write(
-                        '\tAluno "%s" encontrado (%s)' % (aluno.id_aluno.nome_aluno, 'B%d' % num))
                 except TurmaAlunos.DoesNotExist:
                     raise CommandError(
                         'Aluno %s não existe no banco de dados!' % value)
             self.stdout.write(self.style.SUCCESS(
-                '[3/5] Identificados %d alunos' % len(alunos)))
-            self.stdout.write(self.style.WARNING(
-                'Atividades desta turma no banco de dados: %s' % list(map(lambda ativ: ativ.nome, Atividade.objects.filter(
-                    turmaatividades__id_turma=turma, turmaatividades__id_turma__id_periodo=periodo)))))
-            atividades = list()
-            letter = 'C'
-            value =  sheet['%s6' % letter].value
-            while sheet['%s6' % letter].value is not None and sheet['%s6' % letter].value.strip() != '' and sheet['%s6' % letter].value.strip() != 'Total':
-                try:
-                    atividade = TurmaAtividades.objects.get(
-                        id_atividade__nome=value, id_turma=turma)
-                    atividades.append({'ent': atividade, 'pos': letter})
-                    self.stdout.write(
-                        '\tAtividade "%s" encontrada (%s)' % (atividade.id_atividade.nome, '%s6' % num))
-                except TurmaAtividades.DoesNotExist:
-                    Atividade.objects.filter(
-                        turmaatividades__id_turma__nome_turma='CAP201', turmaatividades__id_turma__id_periodo__is_atual=1)
-                    raise CommandError(
-                        'Atividade "%s" não encontrada no banco de dados!' % value)
-                letter = chr(ord(letter) + 1)
-                value =  sheet['%s6' % letter].value
+                '\t[2/6] Identificados %d alunos' % len(alunos)))
+
+            coluna_notas = 'C'
+            celula = sheet['%s6' % coluna_notas].value
+            # O coluna_notas<20 é apenas um sanity check para evitar timeouts
+            while (sheet['%s6' % coluna_notas].value is None or sheet['%s6' % coluna_notas].value.strip() != 'Total') and (ord(coluna_notas) - ord('C')) < 20:
+                coluna_notas = chr(ord(coluna_notas) + 1)
+                celula = sheet['%s6' % coluna_notas].value
+            if celula.strip() != 'Total':
+                raise CommandError(
+                    'Coluna com notas finais não foi encontrada!')
             self.stdout.write(self.style.SUCCESS(
-                '[3/5] Identificadas %d atividades' % len(atividades)))
-            if len(atividades) == 0:
-                self.stdout.write(self.style.WARNING(
-                    '[3/5] Não foram encontradas atividades na planilha nem no banco, criando uma'))
-                atividade = Atividade(data_funcionamento=DatasFuncionamento.objects.get(
-                    data_funcionamento__year=2019, data_funcionamento__month=7, data_funcionamento__day=20), nome='Nota Final', descricao='Lançamento automático', valor_total=100)
-                atividade.save()
-                turmaatividade = TurmaAtividades(
-                    id_turma=turma, id_atividade=atividade)
-                turmaatividade.save()
-                atividades.append({'ent': turmaatividade, 'pos': 'J'})
+                '\t[3/6] Encontrada a coluna com as notas finais'))
+
+            atividade_final = Atividade(data_funcionamento=DatasFuncionamento.objects.get(
+                data_funcionamento__year=2019, data_funcionamento__month=7, data_funcionamento__day=20), nome='Nota Final', descricao='Lançamento automático', valor_total=100)
+            atividade_final.save()
+            turmaatividade = TurmaAtividades(
+                id_turma=turma, id_atividade=atividade_final)
+            turmaatividade.save()
+
             for aluno in alunos:
-                for atividade in atividades:
-                    if NotaAluno.objects.filter(
-                            id_turma_aluno=aluno['ent'], id_atividades_turma=atividade['ent']).exists():
-                        raise CommandError('Aluno "%s" já tem nota(s) lançadas para a atividade "%s"!' % (
-                            aluno['ent'].id_aluno.nome_aluno, atividade['ent'].id_atividade.nome))
-                    nota = NotaAluno(
-                        id_turma_aluno=aluno['ent'], id_atividades_turma=atividade['ent'], valor_nota=sheet['%s%d' % (atividade['pos'], aluno['pos'])].value)
-                    nota.save()
-                    self.stdout.write('\tNota: %s (%d), Aluno: "%s" (%d), Atividade: "%s" (%d)' % (nota.valor_nota, nota.id_nota, nota.id_turma_aluno.id_aluno.nome_aluno,
-                                                                                                   nota.id_turma_aluno.id_aluno.id_aluno, nota.id_atividades_turma.id_atividade.nome, nota.id_atividades_turma.id_atividade.id_atividade))
+                valor_nota = round(
+                    sheet['%s%d' % (coluna_notas, aluno['pos'])].value, 1)
+                nota = NotaAluno(
+                    id_turma_aluno=aluno['ent'], id_atividades_turma=turmaatividade, valor_nota=valor_nota)
+                nota.save()
+                self.stdout.write('\t\tNota: %s (%d), Aluno: "%s" (%d), Atividade: "%s" (%d)' % (nota.valor_nota, nota.id_nota, nota.id_turma_aluno.id_aluno.nome_aluno,
+                                                                                                 nota.id_turma_aluno.id_aluno.id_aluno, nota.id_atividades_turma.id_atividade.nome, nota.id_atividades_turma.id_atividade.id_atividade))
             self.stdout.write(self.style.SUCCESS(
-                '[5/5] Lançadas %d notas' % (len(atividades)*len(alunos))))
+                '\t[4/6] Lançadas %d notas' % len(alunos)))
+
+            outras_notas = NotaAluno.objects.filter(id_turma_aluno__id_turma=turma).exclude(
+                id_atividades_turma__id_atividade=atividade_final)
+            self.stdout.write(self.style.WARNING(
+                '\t[5/6] Deletando %d notas antigas' % outras_notas.count()))
+            outras_notas.delete()
+
+            outras_atividades = TurmaAtividades.objects.filter(
+                id_turma=turma).exclude(id_atividade=atividade_final)
+            self.stdout.write(self.style.WARNING(
+                '\t[6/6] Deletando %d atividades antigas' % outras_atividades.count()))
+            for old_turmaatividade in outras_atividades:
+                old_ativ = old_turmaatividade.id_atividade
+                old_turmaatividade.delete()
+                old_ativ.delete()
 
     def char_range(self, c1, c2):
         """Generates the characters from `c1` to `c2`, inclusive."""
